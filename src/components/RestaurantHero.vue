2a7880257ed47bb1018b3ca3107a0690ba1a7358
eleazer abbey <abbeyeleazer@gmail.com>
2024-10-10 15:21:03 +0200

    created restaurant details page

---
+++ src/components/RestaurantHero.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <div class="relative bg-black/90 rounded-lg flex items-center justify-between h-[29.8rem] px-14">
+      <!-- TEXT  -->
+       <div class="">
+       <div class="text-white">
+        <p class="pb-4">I'm loving it!</p>
+        <h1 class=" text-[3.5rem] text-white font-semibold">McDonald's East London</h1>
+        <div class="flex gap-8 items-center mt-8">
+          <div class="flex items-center justify-center gap-4 border rounded-full py-2 w-[18rem]">
+            <img src="../assets/icons/Order Completed.png" alt="icon">
+            <p> Minimum Order: 12 GBP</p>
+          </div>
+          <div class="flex items-center justify-center gap-4 border rounded-full py-2 w-[18rem]">
+            <img src="../assets/icons/Motocross.png" alt="icon">
+            <p> Delivery in 20-25 Minutes</p>
+          </div>
+        </div>
+       </div>
+       </div>
+
+       <!-- IMAGE -->
+        <div><img src="../assets/images/mcDonald Res.png" alt="hero image"></div>
+        <div class="absolute bottom-[-1.5rem] left-0 w-[20rem] h-14 bg-orange-400 rounded-r-xl flex items-center justify-center gap-4 py-2 text-white">
+          <img src="../assets/icons/Clock.png" alt="clock icon">
+          <p>Open until 3:00 AM</p>
+        </div>
+      </div>
+
+  </header>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RestaurantDetailViewVue from '@/views/RestaurantDetailView.vue.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,9 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/menu',
-      name: 'menu',
-      component: () => import('../views/RestaurantDetailView.vue.vue')
+      path: '/restaurants',
+      name: 'restaurants',
+      component: RestaurantDetailViewVue
     }
   ]
 })
